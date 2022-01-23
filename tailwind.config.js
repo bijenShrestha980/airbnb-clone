3089d0c742c75c8a733a3dd02016c500f8e6e0c7
bijenShrestha980 <s.bijen93@gmail.com>
2022-01-24 00:15:16 +0545

    search page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   content: [
     "./pages/index.js",
+    "./pages/search.js",
+
     "./components/Header.js",
     "./components/Banner.js",
     "./components/SmallCard.js",
     "./components/MediumCard.js",
     "./components/LargeCard.js",
     "./components/Footer.js",
+    "./components/InfoCard.js",
   ],
   theme: {
     extend: {},
